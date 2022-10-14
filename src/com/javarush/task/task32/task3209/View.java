5d552ad0db5c5d9d9287f89e703a01546faa2b7a
yrkash <yrkash@gmail.com>
2022-10-14 16:35:05 +0300

    changes in View.java

--- src/com/javarush/task/task32/task3209/View.java
+++ src/com/javarush/task/task32/task3209/View.java
@@ -1,5 +1,7 @@
 package com.javarush.task.task32.task3209;
 
+import com.javarush.task.task32.task3209.listeners.FrameListener;
+
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -13,7 +15,25 @@ public class View extends JFrame implements ActionListener {
     private JEditorPane plainTextPane  = new JEditorPane();
 
     public void init(){
+        initGui();
+        FrameListener listener = new FrameListener(this);
+        addWindowListener(listener);
+        setVisible(true);
+    }
 
+    public void initMenuBar() {
+
+    }
+
+    public void initEditor() {
+
+    }
+
+
+    public void initGui() {
+        initMenuBar();
+        initEditor();
+        pack();
     }
 
     public Controller getController() {
